fa862f39993692d77fcdf3c942d2c030882db27e
Nathan Dorazil <nathand1915@gmail.com>
2020-06-06 03:37:06 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+  
+  # determining whether the array contains any false elements
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
+
